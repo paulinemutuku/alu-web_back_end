475160f2d6800caae286969607781e7ee7d37d37
paulinemutuku <p.mutuku@alustudent.com>
2024-09-06 05:56:36 -0700

    variable annotations

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    Type annotated function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+        Args:
+            a: The first parameter
+            b: The second parameter
+
+        Return:
+            Sum of two floats
+
+    """
+
+    return (a + b)
