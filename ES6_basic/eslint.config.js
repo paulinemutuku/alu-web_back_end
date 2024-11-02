d54119c0284e5cefe03ac30e5d85abd50a75956e
Pauline <p.mutuku@alustudent.com>
2024-11-02 12:10:14 +0800

    block scope

---
+++ ES6_basic/eslint.config.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+      files: ['**/*.js'],
+      languageOptions: {
+        ecmaVersion: 'latest',
+      },
+      rules: {
+        // Add your ESLint rules here if needed
+      },
+    },
+  ];
+  
